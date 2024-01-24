5af96cb7019de861c75225c7bf630ae141a8b36b
cyj127 <rkdgudtjd112@naver.com>
2024-01-24 16:53:45 +0900

    boolean 입력

---
+++ Ch02_Variable/src/ch02/Operator04/CompOperator.java
@@ -0,0 +1,19 @@
+package ch02.Operator04;
+
+public class CompOperator {
+	public static void main(String[] args) {
+		int num0 = 100;
+		int num1 = 10;
+		int num2 = 100;
+		
+		boolean bVal = num0 > num1;
+		System.out.println(bVal);
+		bVal = num0 < num1;
+		System.out.println(bVal);
+		bVal = num0 == num1;
+		System.out.println(bVal);
+	    bVal = num0 >= num2;
+	    System.out.println(bVal);
+	}
+
+}
