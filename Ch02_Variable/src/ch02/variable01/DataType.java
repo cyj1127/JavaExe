d6d449effa6b56bb9e89d90d0a9f3982b5d3a03b
cyj127 <rkdgudtjd112@naver.com>
2024-01-24 16:54:11 +0900

    Data type 입력

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,22 @@
+package ch02.variable01;
+
+public class DataType {
+	public static void main(String[] args) {
+		//자료형 변수공간이름 = 값
+		int num = 100;
+		double dnum = 3.14;
+		char ch = 'a'; // 문자 1글자
+		String name = "홍길동";
+		boolean isRun = true;
+		String str = "a";
+		
+		System.out.println(num);
+		num = 999;
+		System.out.println(dnum);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(isRun);
+		System.out.println(str);
+	}
+
+}
