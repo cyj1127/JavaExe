7cdaeb897631be7fb8791567e0afab126c1ca06f
cyj127 <rkdgudtjd112@naver.com>
2024-01-25 20:59:25 +0900

    15 임시저장

---
+++ Ch03_Control/src/ch03/for10/Resolve15.java
@@ -0,0 +1,24 @@
+package ch03.for10;
+
+import java.util.Scanner;
+
+/*
+ * 15. 사용자로부터 정수를 입력받습니다.
+   입력 받은 값을 계속 더합니다
+   사용자가 0을 입력하면 합을 출력합니다
+   프로그램을 종료합니다.
+ */
+public class Resolve15 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int num ;
+		int sum;
+		do {
+		System.out.println("정수를 입력하세요");
+
+		num = sc.nextInt();
+		sum+= i
+		}while(num ==0);
+		sc.close();
+			}
+		}
