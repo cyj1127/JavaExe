11619a8ce255db795294d3eb7fbdd316438ab401
cyj127 <rkdgudtjd112@naver.com>
2024-01-25 17:40:16 +0900

    tutor 소스 업로드

--- Ch02_Variable/src/ch02/Operator04/CompOperator.java
+++ Ch02_Variable/src/ch02/Operator04/CompOperator.java
@@ -6,13 +6,13 @@ public class CompOperator {
 		int num1 = 10;
 		int num2 = 100;
 		
-		boolean bVal = num0 > num1;
+		boolean bVal = num0 > num1; // 100 > 10
+		System.out.println(bVal);	// true
+		bVal = num0 < num1;  // 100 < 10
+		System.out.println(bVal);	// false
+		bVal = num0 == num2;	// 100 == 100
 		System.out.println(bVal);
-		bVal = num0 < num1;
-		System.out.println(bVal);
-		bVal = num0 == num1;
-		System.out.println(bVal);
-	    
+		bVal = num0 >= num2; // 100 >= 100
+		System.out.println(bVal);	// true
 	}
-
 }
--- Ch02_Variable/src/ch02/input03/Operator.java
+++ Ch02_Variable/src/ch02/input03/Operator.java
@@ -7,24 +7,31 @@ public class Operator {
 		int result = 0;
 		int num0 = 0, num1 = 0;
 		
-		//라이브러리에(JDK를 포함한)에 존재하는 클래스는
-		//Ctrl + Shift + O 를 누르면 자동으로 PAKAGE 경로를 등록해준다.
-		//JDK에서 제공, 키보드에서 입력을 받을 때 사용
+		// 라이브리리(jdk를 포함한)에 존재하는 클래스는
+		// Ctrl + Shift + o를 누르면 자동으로
+		// package경로를 등록해준다.
+		// jdk에 있는 클래스이고 키보드 입력을 받을 때 사용한다.
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("더하기 연산을 합니다.");
-		System.out.println("1번째 숫자 입력 >>");
+		System.out.println("더하기 연산을 합니다");
+		System.out.print("1번째 숫자 입력 >> ");
 		num0 = sc.nextInt();
-		System.out.println("2번째 숫자 입력 >>");
+		System.out.print("2번째 숫자 입력 >> ");
 		num1 = sc.nextInt();
 		
-		//결과 계산
-		// =(대입 연산자) 는 우측의 연산이 모두 완료된 후 처리된다.
+		// 결과 계산
+		// =(대입 연산자)는 우측의 연산이 모두 완료된 후 처리된다.
 		result = num0 + num1;
-		System.out.println("결과는" + result);
+		System.out.println("결과는 " + result);
 		
-		
-		sc.close(); //키보드 입력을 그만 받을 때 사용(연결이 끊어짐)
+		sc.close(); 	// 키보드와 연결을 닫는다.
 	}
-
 }
+
+
+
+
+
+
+
+
